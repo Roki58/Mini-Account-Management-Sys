dcbffc65e3525aefcef416a5f491d7c636ba3734
Roki <ahosanulroki@gmail.com>
2025-06-04 01:35:53 +0600

    implement edit  options

--- MiniAccManagementSys/Pages/Account/Edit.cshtml.cs
+++ MiniAccManagementSys/Pages/Account/Edit.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MiniAccManagementSys.Models;
+using MiniAccManagementSys.Services;
+using System.Threading.Tasks;
 
 namespace MiniAccManagementSys.Pages.Account
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly AccountService _accountService;
+
+        public EditModel(AccountService accountService)
         {
+            _accountService = accountService;
+        }
+
+        [BindProperty]
+        public ChartOfAccount Account { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Account = await _accountService.GetAccountByIdAsync(id);
+            if (Account == null) return NotFound();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+          
+            await _accountService.SaveAccountAsync(Account, "Update");
+            return RedirectToPage("/Index");  
         }
     }
 }
--- MiniAccManagementSys/obj/Debug/net8.0/MiniAccManagementSys.AssemblyInfo.cs
+++ MiniAccManagementSys/obj/Debug/net8.0/MiniAccManagementSys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniAccManagementSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd76d70f87aed57a307982719ce9c41fa9df0c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f26201ebb50023bbfc3900b63ea127daea53803")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniAccManagementSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniAccManagementSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
